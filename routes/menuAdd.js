91d68a4bf15c6ff3cb030637445d15d37985dbd1
monishkumar1999 <monishkumarapj@gmail.com>
2024-12-19 18:36:34 +0530

    Update main menu

--- routes/menuAdd.js
+++ routes/menuAdd.js
@@ -6,7 +6,7 @@ const menuRouter = express.Router();
 menuRouter.use("/insertMainMenu", (req, res) => {
   const menu = new Menu(req.body);
   menu.save();
-  res.send("Main menu created")
+  res.send("Main menu created");
 });
 
 menuRouter.post("/addSubmenu", async (req, res) => {
@@ -24,7 +24,7 @@ menuRouter.post("/addSubmenu", async (req, res) => {
 
     menu.submenu = true;
 
-    menu.submenuItems.push(...insertsubmenus);  
+    menu.submenuItems.push(...insertsubmenus);
     const updated = await menu.save();
     if (updated) {
       res.status(200).send(updated);
@@ -34,13 +34,44 @@ menuRouter.post("/addSubmenu", async (req, res) => {
   }
 });
 
-
 menuRouter.get("/showMenu", async (req, res) => {
   try {
-   const data= await Menu.find({})
-     res.send(data)
+    const data = await Menu.find({});
+    res.send(data);
   } catch (err) {
     res.status(400).send(err.message);
+  }
+});
+
+menuRouter.post("/delete/MainMenu", async (req, res) => {
+  const title = req.body.Title;
+
+  const deleted = await Menu.deleteOne({ title: title });
+  res.send(req.body);
+});
+
+menuRouter.post("/update/MainMenu", async (req, res) => {
+  try{
+    const { mainMenuName ,updateName} = req.body;
+  const menu = await Menu.findOne({ title: mainMenuName });
+
+  if (!menu) {
+    res.status(404).json({ status: true, message: "No data" });
+    return
+  }
+
+   menu.title= updateName
+  await menu.save()
+  res.json({
+    "status":"true",
+    "message":"update successfully"
+  });
+  }
+  catch(err){
+  res.status(400).json({
+    "status":"false",
+    "message":err.message
+  })
   }
 });
 
